a7eb1144d34f8aa2789f7f8877af2b59ed34dd04
Oreki13 <arfandynugraha21@gmail.com>
2019-09-11 16:07:19 +0700

    print finish

--- src/Components/CartBar/CartBar.js
+++ src/Components/CartBar/CartBar.js
@@ -1,31 +1,32 @@
 import React, { Component } from "react";
-import {connect} from 'react-redux'
-import {getCart, deleteCart} from '../../Publics/Redux/Actions/cart'
+import { connect } from "react-redux";
+import { getCart, deleteCart } from "../../Publics/Redux/Actions/cart";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import "./CartBar.css";
-import ReactToPrint from "react-to-print";
-import ModalOut from "../Modal/Checkout";
 
+import ModalOut from "../Modal/Checkout";
 
 class CartBar extends React.Component {
   state = {
     show: false,
-    cart:[]
+    cart: []
   };
 
-componentDidMount = async () => {
-     await this.props.dispatch(getCart(1))
-     this.setState({cart : this.props.cart})
-   }
+  componentDidMount = async () => {
+    await this.props.dispatch(getCart(1));
+    this.setState({ cart: this.props.cart });
+  };
 
-   handleRemove = async (id_user, id_course) => {
-     await this.props.dispatch(deleteCart(id_user, id_course)).then(() => {
-       const cart = this.state.cart.filter(cart => (cart.id_user && cart.id_course) !== (id_user && id_course))
-       this.setState({cart})
-     })
-   }
+  handleRemove = async (id_user, id_course) => {
+    await this.props.dispatch(deleteCart(id_user, id_course)).then(() => {
+      const cart = this.state.cart.filter(
+        cart => (cart.id_user && cart.id_course) !== (id_user && id_course)
+      );
+      this.setState({ cart });
+    });
+  };
 
-   handleMoveWishlist = async (id_user, id_course) => {
+  handleMoveWishlist = async (id_user, id_course) => {
     //  POST KE WISHLIST
     // await this.props.dispatch(postWishlist(id_user, id_course)).then(() => {
     //   SETELAH BERHASIL DI POST LALU DELETE DATA YG ADA DI CART
@@ -34,8 +35,7 @@ componentDidMount = async () => {
     //     this.setState({cart})
     //   })
     // })
-   }
-
+  };
 
   handleClose = () => {
     this.setState({ show: false });
@@ -45,58 +45,87 @@ componentDidMount = async () => {
   };
 
   render() {
-    console.log('props', this.props)
-    let total = 0
+    // console.log("props", this.props);
+    let total = 0;
     this.state.cart.map(course => {
-      total += course.price
-    })
+      total += course.price;
+    });
+    console.log("cart", this.state.cart);
     return (
       // ref={ref => (this.printOut = ref)}
       <div>
         <Container>
           <Row className="cart-bar">
             <Col md={9} style={{ textAlign: "left" }}>
-              <h5>{ this.state.cart.length } Course in cart</h5>
-      {this.state.cart.map(course => (
-              <Row>
-                <Col sm={12}>
-                  <Card>
-                    <Card.Body className="cart-body">
-                      <Row>
-                        <Col xs={3}>
-                          <Card.Img
-                            className="body-image"
-                            variant="top"
-                            src={course.image}
-                          />
-                        </Col>
-                        <Col xs={4} className="body-text">
-                          <h6>{ course.title }</h6>
-                        </Col>
-                        <Col xs={3} className="body-cmd">
-                          <span>
-                            <a style={{color:'blue', cursor:'pointer'}} onClick={() => this.handleRemove(course.id_user, course.id_course)}>Remove</a>
-                            
-                          </span>
+              <h5>{this.state.cart.length} Course in cart</h5>
+              {this.state.cart.map(course => (
+                <Row>
+                  <Col sm={12}>
+                    <Card>
+                      <Card.Body className="cart-body">
+                        <Row>
+                          <Col xs={3}>
+                            <Card.Img
+                              className="body-image"
+                              variant="top"
+                              src={course.image}
+                            />
+                          </Col>
+                          <Col xs={4} className="body-text">
+                            <h6>{course.title}</h6>
+                          </Col>
+                          <Col xs={3} className="body-cmd">
+                            <span>
+                              <a
+                                style={{ color: "blue", cursor: "pointer" }}
+                                onClick={() =>
+                                  this.handleRemove(
+                                    course.id_user,
+                                    course.id_course
+                                  )
+                                }
+                              >
+                                Remove
+                              </a>
+                            </span>
 
-                          <div>
-                            <a style={{color:'blue', cursor:'pointer'}} onClick={() => this.handleMoveWishlist(course.id_user, course.id_course)}>Move to Wishlist</a>
-                          
-                          </div>
-                        </Col>
-                        <Col xs={2} className="price">
-                          <b>Rp  {course.price.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</b>
-                        </Col>
-                      </Row>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
-))}
+                            <div>
+                              <a
+                                style={{ color: "blue", cursor: "pointer" }}
+                                onClick={() =>
+                                  this.handleMoveWishlist(
+                                    course.id_user,
+                                    course.id_course
+                                  )
+                                }
+                              >
+                                Move to Wishlist
+                              </a>
+                            </div>
+                          </Col>
+                          <Col xs={2} className="price">
+                            {console.log(total)}
+                            <b>
+                              Rp{" "}
+                              {course.price.toLocaleString(navigator.language, {
+                                minimumFractionDigits: 0
+                              })}
+                            </b>
+                          </Col>
+                        </Row>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                </Row>
+              ))}
             </Col>
             <Col md={3} className="cart-checkout">
               <h5>Total</h5>
-              <h3>Rp.{total.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</h3>
+              <h3>
+                {total.toLocaleString(navigator.language, {
+                  minimumFractionDigits: 0
+                })}
+              </h3>
               {/* <ReactToPrint
                 trigger={() => (
                   <Button variant="danger" size="lg" block>
@@ -119,17 +148,17 @@ componentDidMount = async () => {
           keyClose={this.handleClose}
           keyOpen={this.handleShow}
           keys={this.state.show}
+          datas={this.state.cart}
+          total={total}
         />
       </div>
     );
   }
 }
-  const mapStateToProps = state => {
-    return {
-      cart : state.cart.cart
-    }
-  }
-
-
+const mapStateToProps = state => {
+  return {
+    cart: state.cart.cart
+  };
+};
 
 export default connect(mapStateToProps)(CartBar);
--- src/Components/Modal/Checkout.js
+++ src/Components/Modal/Checkout.js
@@ -1,43 +1,59 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button, Modal, Table } from "react-bootstrap";
+import ReactToPrint from "react-to-print";
 
 function ModalOut(props) {
+  const componentRef = useRef();
+
   return (
     <>
-      <Modal show={props.keys} onHide={props.keyClose}>
+      <Modal show={props.keys} onHide={props.keyClose} size="lg">
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td colSpan="2">Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </Table>
+          <div ref={componentRef}>
+            <Table striped bordered hover size="lg">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Title</th>
+                  <th>Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {props.datas.map(data => (
+                  <tr>
+                    <td>1</td>
+                    <td>{data.title}</td>
+                    <td>
+                      Rp.
+                      {data.price.toLocaleString(navigator.language, {
+                        minimumFractionDigits: 0
+                      })}
+                    </td>
+                  </tr>
+                ))}
+                <tr>
+                  <td>Total</td>
+                  <td colSpan="2" className="text-right">
+                    Rp.
+                    {props.total.toLocaleString(navigator.language, {
+                      minimumFractionDigits: 0
+                    })}
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+          <ReactToPrint
+            trigger={() => (
+              <Button variant="danger" className="float-right">
+                Checkout
+              </Button>
+            )}
+            content={() => componentRef.current}
+          />
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={props.keyClose}>
